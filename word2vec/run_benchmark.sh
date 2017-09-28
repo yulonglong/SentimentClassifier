32a01cd2ac2c6d3d267676a90e2a31c5e3c11641
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2017-09-28 17:34:12 +0800

    Added word2vec binaries

---
+++ word2vec/run_benchmark.sh
@@ -0,0 +1,4 @@
+date
+./word2vec -train corpus/1-billion-lm-benchmark/train.txt -output vectors/1-billion-lm-benchmark.300.w2v.txt -size 300 -hs 1 -cbow 0 -threads 14
+date
+
---
+++ word2vec/vectors/readme.txt
@@ -0,0 +1,4 @@
+Folder to put all the pre-trained word embeddings
+For example, download GloVe: http://nlp.stanford.edu/data/glove.6B.zip
+And extract the zip here
+
