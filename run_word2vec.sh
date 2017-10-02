ebe98d38dfc652702d8cf25c17c14f891fba6025
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2017-10-02 16:18:11 +0800

    Added log printing for word2vec script

--- run_word2vec.sh
+++ run_word2vec.sh
@@ -18,14 +18,18 @@ if [ ! -f word2vec/vectors/imdb_${embedding_size}d.w2v.txt ]; then
     if [ ! -f word2vec/vectors/${combined_tokenized_text_name} ]; then
         # If combined text file is not present
         if [ ! -f word2vec/vectors/${combined_text_name} ]; then
+            echo "Combining dataset into one text file..."
             # Compile the combine text script
             g++ word2vec/combineText4word2vec.cpp -o word2vec/combineText4word2vec.out
             # Execute combine text script
             ./word2vec/combineText4word2vec.out data/aclImdb/train/unsup/ word2vec/vectors/${combined_text_name}
         fi
+        echo "Tokenizing text..."
         # Tokenize text
         python core/tokenizeText.py -in word2vec/vectors/${combined_text_name} -out word2vec/vectors/${combined_tokenized_text_name}
     fi
     # Train word2vec
+    echo "Training word2vec"
     ./word2vec/word2vec -train word2vec/vectors/${combined_tokenized_text_name} -output word2vec/vectors/imdb_${embedding_size}d.w2v.txt -size ${embedding_size} -hs 1 -cbow 0 -threads 6
 fi
+echo "Done!"
