696e1c5bb07cbb64ed1c80618f710caa9370842f
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2018-03-16 12:36:16 +0800

    Added scikit-learn and nltk punkt in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,11 @@
 
 pip install numpy==1.13.3 --user
 pip install scipy==0.19.1 --user
+pip install scikit-learn==0.19.1 --user
 pip install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp27-cp27mu-manylinux1_x86_64.whl --user
 pip install torchvision --user
 pip install nltk==3.2.2 --user
 pip install pydot==1.1.0 --user
 pip install h5py==2.6.0 --user
 pip install matplotlib==1.5.3 --user
-
+python -m nltk.downloader 'punkt'
