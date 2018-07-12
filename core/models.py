86b45b91703578be35de93e225cf48dacf5115ea
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2018-07-12 16:11:50 +0800

    Updated PyTorch and other libraries to latest version

--- core/models.py
+++ core/models.py
@@ -79,12 +79,12 @@ class Attention(nn.Module):
         self.v.data.uniform_(-stdv, stdv)
 
     def forward(self, input):
-        wplus = torch.mm(input.view(-1, input.size()[2]), self.w)
-        wplus = wplus.view(-1, input.size()[1], self.w.size()[1])
+        wplus = torch.mm(input.contiguous().view(-1, input.size()[2]), self.w)
+        wplus = wplus.contiguous().view(-1, input.size()[1], self.w.size()[1])
         wplus = F.tanh(wplus)
 
-        att_w = torch.mm(wplus.view(-1, wplus.size()[2]), self.v.view(self.v.size()[0], 1))
-        att_w = att_w.view(-1, wplus.size()[1])
+        att_w = torch.mm(wplus.contiguous().view(-1, wplus.size()[2]), self.v.contiguous().view(self.v.size()[0], 1))
+        att_w = att_w.contiguous().view(-1, wplus.size()[1])
         att_w = F.softmax(att_w,dim=1)
 
         # Save attention weights to be retrieved for visualization
--- setup_global.sh
+++ setup_global.sh
@@ -5,13 +5,13 @@ echo "WARNING: This script assumes you already have pip and java JDK installed"
 echo "If not, please execute the following command:"
 echo "sudo apt-get update && sudo apt-get install python-pip openjdk-8-jdk"
 
-sudo pip install numpy==1.14.2
-sudo pip install scipy==1.0.0
+sudo pip install numpy==1.14.5
+sudo pip install scipy==1.1.0
 sudo pip install scikit-learn==0.19.1
-sudo pip install http://download.pytorch.org/whl/cu91/torch-0.3.1-cp27-cp27mu-linux_x86_64.whl 
-sudo pip install torchvision
-sudo pip install nltk==3.2.5
+sudo pip install http://download.pytorch.org/whl/cu91/torch-0.4.0-cp27-cp27mu-linux_x86_64.whl 
+sudo pip install torchvision==0.2.1
+sudo pip install nltk==3.3.0
 sudo pip install pydot==1.2.4
-sudo pip install h5py==2.7.1
-sudo pip install matplotlib==2.2.0
+sudo pip install h5py==2.8.0
+sudo pip install matplotlib==2.2.2
 python -m nltk.downloader 'punkt'
